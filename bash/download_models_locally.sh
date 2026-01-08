041321e1b5b23222eaf89d7b9c475cbe7b0644ec
eryk <eryk.mikolajewicz@gmail.com>
2026-01-08 22:57:31 +0100

    Adjust bash model downloading script, to reflect current quantization options.

--- bash/download_models_locally.sh
+++ bash/download_models_locally.sh
@@ -1,6 +1,5 @@
+.venv/bin/python ./building/onnx/model_download.py --environment=local_test
 .venv/bin/python ./building/onnx/model_download.py --quantization=fp16 --environment=local_test
 .venv/bin/python ./building/onnx/model_download.py --quantization=int8 --environment=local_test
 .venv/bin/python ./building/onnx/model_download.py --quantization=int4 --environment=local_test
 .venv/bin/python ./building/sentence_transformers/model_download.py --environment=local_test
-.venv/bin/python ./building/sentence_transformers/model_download.py --quantization=int8 --environment=local_test
-.venv/bin/python ./building/sentence_transformers/model_download.py --quantization=int4 --environment=local_test
