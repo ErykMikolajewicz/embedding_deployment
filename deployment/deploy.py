800e43e95a342ae917b33c2193e70d4bff98c0d0
eryk <eryk.mikolajewicz@gmail.com>
2026-01-13 22:27:04 +0100

    Delete deployment part. It not give any gain, since using testcontainers, to build images, and unnecessarily increase project complexity

--- deployment/deploy.py
+++
@@ -1,7 +0,0 @@
-from deployment.src.infrastructure.image_builders import get_image_builder
-from deployment.src.settings import deployment_settings
-
-
-image_builder = get_image_builder(deployment_settings.DEPLOYMENT_OPTION)
-
-image_builder.build_image(deployment_settings.QUANTIZATION)
